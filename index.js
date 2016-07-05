24d99e6199a4b05e95cff6385254c119858e6171
Andrew Mestas <andrew.mestas.dev@gmail.com>
2016-07-05 14:05:24 -0700

    stuff testing

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ app.get('/', function(req, res) {
   res.send("Hello World");
 });
 
+
+
+app.post('/view', function(req, res) {
+  res.send("view");
+});
+
+app.post('/copy', function(req, res) {
+  res.send("copy");
+});
+
+app.post('/paste', function(req, res) {
+  res.send("paste");
+});
+
+app.post('/delete', function(req, res) {
+  res.send("delete");
+});
+
+
 app.listen(3000, function() {
   console.log("Listening on port 3000...");
 });
