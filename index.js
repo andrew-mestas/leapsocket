296e8a6d97d20e21ae6b2de1f3e0d54ffca56374
Andrew Mestas <andrew.mestas.dev@gmail.com>
2016-07-05 15:17:25 -0700

    we got the file system

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser').json();
-
+var fs = require('fs');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -10,10 +10,35 @@ app.get('/', function(req, res) {
 });
 
 
-
 app.post('/view',bodyParser, function(req, res) {
-	console.log(req.body.value);
-  res.send("view");
+
+  	var isFileOrDir = function(filenameStr){ 
+	 	var fileStat = fs.statSync(filenameStr);
+	 	if(fileStat.isFile()){
+	 		return {type: "file", name : filenameStr}
+		} else if(fileStat.isDirectory()){
+			return {type: "folder", name : filenameStr}
+		}
+  	}
+
+	fs.readdir("/", function(err, files){
+	var root = "/";
+	var currentDir = root ;
+  	var filesAndFolders = [];
+  	files.forEach(function(filenameStr){
+  		console.log(filenameStr)
+ 		root += filenameStr;
+		filesAndFolders.push(isFileOrDir(root));
+
+ 		root = "/";
+
+  	})
+
+  	res.send({current: currentDir, filesAndFolders : filesAndFolders})
+  	});
+
+
+	// });
 });
 
 app.post('/copy', function(req, res) {
@@ -27,6 +52,19 @@ app.post('/paste', function(req, res) {
 app.post('/delete', function(req, res) {
   res.send("delete");
 });
+
+app.post('/make', function(req, res) {
+  res.send("make");
+});
+
+app.post('/move', function(req, res) {
+  res.send("move");
+});
+
+app.post('/rename', function(req, res) {
+  res.send("rename");
+});
+
 
 
 app.listen(3000, function() {
